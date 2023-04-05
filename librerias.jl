ef2d3e3b0527ffaf35b5c591c8b652b6975284fe
Rodrigo Naranjo González <56093078+rng1@users.noreply.github.com>
2023-04-06 00:59:31 +0200

    Add files via upload

---
+++ librerias.jl
@@ -0,0 +1,3 @@
+using Statistics # Funciones estadísticas comunes para análisis de datos.
+using DelimitedFiles # Herramientas para leer y escribir archivos delimitados.
+using DataFrames # Wstructuras de datos para manipular datos tabulares en Julia.
