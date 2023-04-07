69546d7a33c59e93b074cb973004993d09a4e176
Rodrigo Naranjo González <56093078+rng1@users.noreply.github.com>
2023-04-07 02:20:45 +0200

    Avanzado el boletín 2
    Se han añadido las funciones para crear la RNA, calcular la precisión, clasificar las salidas, y cambiado cosas varias en las funciones existentes de normalización. Se ha añadido el paquete Flux a las librerías utilizadas.

--- exercises/bol1.jl
+++ exercises/bol1.jl
@@ -1,6 +1,8 @@
-include("libraries.jl")
+include("librerias.jl")
 
-function one_hot_encoding(feature::AbstractArray{<:Any,1}, classes)
+# Codificación
+
+function one_hot_encoding(feature::AbstractArray{<:Any,1}, classes::AbstractArray{<:Any,1})
     """
     Recibe el vector con las variables categóricas y las clases únicas que pueden darse.
     En el caso de tener dos categorías devuelve un vector de booleanos,
@@ -20,59 +22,189 @@ function one_hot_encoding(feature::AbstractArray{<:Any,1}, classes)
     return bool_matrix
 end
 
-function std_norm_params(inputs::AbstractArray{<:Real,2})
+"""
+Función sobrecargada de one_hot_encoding, que extraiga directamente las categorías únicas de salida.
+"""
+one_hot_encoding(feature::AbstractArray{<:Any,1}) = one_hot_encoding(feature::AbstractArray{<:Any,1}, unique(feature))
+
+"""
+Función sobrecargada de one_hot_encoding, que convierte un vector con entradas binarias a una matriz 
+de salida con una salida única.
+"""
+one_hot_encoding(feature::AbstractArray{Bool,1}) = reshape(feature, length(feature), 1)
+
+# Normalización con media y desviación típica
+
+function calculate_zeromean_normalization_parameters(inputs::AbstractArray{<:Real,2})
     """
     Devuelve la media y desviación típica de cada atributo para un vector de dos dimensiones.
     """
-    means = mean(inputs, dims=1)
-    stds = std(inputs, dims=1)
+    means = mean(inputs, dims = 1)
+    stds = std(inputs, dims = 1)
 
-    return means, stds
+    return (means, stds)
 end
 
-function std_norm(inputs::AbstractArray{<:Real,2})
+function normalize_zeromean!(inputs::AbstractArray{<:Real,2}, meanstd::NTuple{2,AbstractArray{<:Real,2}})
     """
     Realiza la normalización mediante media y desviación típica.
     En el caso de que la desviación típica sea 0, el valor queda en 0.
     """
-    means, stds = std_norm_params(inputs)
-    normalized_inputs = (inputs .- means)./stds
+    normalized_inputs = (inputs .- meanstd[1])./meanstd[2]
 
     for i = 1:size(inputs,2)
-        if stds[i] == 0
+        if meanstd[2][i] == 0
             normalized_inputs[:, i] .= 0
         end
     end
 
     return normalized_inputs
+end
+
+"""
+Función sobrecargada de normalize_zeromean!, con un único parámetro que calcule los parámetros
+de normalización automáticamente.
+"""
+normalize_zeromean!(inputs::AbstractArray{<:Real,2}) = normalize_zeromean!(inputs, calculate_zeromean_normalization_parameters(inputs))
+
+function normalize_zeromean(inputs::AbstractArray{<:Real,2}, meanstd::NTuple{2, AbstractArray{<:Real,2}})
+    """
+    Misma función que normalize_zeromean! pero sin modificar la matriz de datos.
+    """
+    return normalize_zeromean!(copy(inputs), meanstd)
 end;
 
-function minmax_norm_params(inputs::AbstractArray{<:Real,2})
+"""
+Misma función sobrecargada que normalize_zeromean! pero sin modificar la matriz de datos.
+"""
+normalize_zeromean(inputs::AbstractArray{<:Real,2}) = normalize_zeromean!(copy(inputs));
+
+# Normalización con máximo y mínimo
+
+function calculate_minmax_normalization_parameters(inputs::AbstractArray{<:Real,2})
     """
     Devuelve el máximo y mínimo de cada atributo para un vector de dos dimensiones.
     """
     min = minimum(inputs, dims = 1)
     max = maximum(inputs, dims = 1)
 
-    return min, max
+    return (min, max)
 end
 
-function minmax_norm(inputs::AbstractArray{<:Real,2})
+function normalize_minmax!(inputs::AbstractArray{<:Real,2}, minmax::NTuple{2,AbstractArray{<:Real,2}})
     """    
     Realiza la normalización usando el valor mínimo y el máximo.
     En el caso de que el mínimo sea igual al máximo, se añade un valor despreciable
     que evite la división entre 0.
     """
-    min, max = minmax_norm_params(inputs)
-    normalized_inputs = (inputs .- max)./(min .- max .+ 1e-8 * (min == max))
+    normalized_inputs = (inputs .- minmax[2])./(minmax[1] .- minmax[2] .+ 1e-8 * (minmax[1] == minmax[2]))
 
     return normalized_inputs
+end
+
+"""
+Función sobrecargada de normalize_minmax!, con un único parámetro que calcule los parámetros
+de normalización automáticamente.
+"""
+normalize_minmax!(inputs::AbstractArray{<:Real,2}) = normalize_minmax!(inputs, calculate_minmax_normalization_parameters(inputs))
+
+function normalize_minmax(inputs::AbstractArray{<:Real,2}, minmax::NTuple{2, AbstractArray{<:Real,2}})
+    """
+    Misma función que normalize_minmax! pero sin modificar la matriz de datos.
+    """
+    return normalize_minmax!(copy(inputs), minmax)
 end;
+
+"""
+Misma función sobrecargada que normalize_minmax! pero sin modificar la matriz de datos.
+"""
+normalize_minmax(inputs::AbstractArray{<:Real,2}) = normalize_minmax!(copy(inputs));
+
+# Clasificar salidas
+
+function classify_outputs(outputs::AbstractArray{<:Real,2}, threshold = 0.5)
+    """
+    Recibe como entrada una matriz de salidas de un modelo y devuelve una matriz de booleanos
+    del mismo tamaño, donde cada fila tiene un único valor true que indica la clase a la que se 
+    clasifica ese patrón.
+    """
+    n_cols = size(outputs, 2)
+
+    if n_cols == 1
+        return outputs .>= threshold
+    else
+        (_, indices_max_each_instance) = findmax(outputs, dims = 2)
+        outputs = falses(size(outputs))
+        outputs[indices_max_each_instance] = outputs[indices_max_each_instance] .= true
+    end
+
+    return outputs
+end
+
+# Precisión
+
+"""
+Calcula el promedio de los vectores booleanos targets y outputs, resultando en la precisión.
+"""
+accuracy(targets::AbstractArray{Bool,1}, outputs::AbstractArray{Bool,1}) = mean(outputs .== targets)
+
+function accuracy(targets::AbstractArray{Bool,2}, outputs::AbstractArray{Bool,2})
+    """
+    Función sobrecargada que usa matrices bidimensionales de valores booleanos.
+    """
+    n_cols = size(outputs, 2)
+
+    if n_cols == 1
+        return accuracy(targets[:,1], outputs[:,1])
+    else
+        class_comparison = targets .== outputs
+        correct_classifications = all(class_comparison, dims = 2)
+        return mean(correct_classifications)
+    end
+end
+
+"""
+Función sobrecargada donde las salidas no se han interpretado como valores booleanos.
+"""
+accuracy(targets::AbstractArray{Bool,1}, outputs::AbstractArray{<:Real,1}, threshold = 0.5) = 
+    accuracy((outputs .>= threshold), targets)
+
+"""
+Función sobrecarga donde las salidas reales no se han interpretado todavía como valores
+de pertenencia a N clases.
+"""
+function accuracy(targets::AbstractArray{Bool,2}, outputs::AbstractArray{<:Real,2})
+    if size(outputs, 2) == 1
+        return accuracy(targets[:,1], outputs[:,1])
+    else
+        return accuracy(targets, classify_outputs(outputs))
+    end
+end
+
+# RNA
+
+function rna(topology::AbstractArray{<:Int,1}, n_inputs, n_outputs)
+    ann = Chain();
+    n_inputs_layer = n_inputs
+
+    for n_outputs_layer = topology
+        ann = Chain(ann..., Dense(n_inputs_layer, n_outputs_layer, σ) );
+        n_inputs_layer = n_outputs_layer;
+    end
+    
+    n_outputs <= 2 ? 
+        ann = Chain(ann...,  Dense(n_inputs_layer, 1, σ)) : 
+        ann = Chain(ann...,  Dense(n_inputs_layer, n_outputs, identity), softmax)
+    
+    return ann
+end;
+
+# Dataset
 
 dataset = readdlm("iris.data", ',')
 targets = dataset[:, 5]
 targets_classes = unique(targets)
 
 inputs = Float32.(dataset[:, 1:4])
-normalized_inputs = std_norm(inputs)
+normalized_inputs = normalize_zeromean!(inputs, calculate_minmax_normalization_parameters(inputs))
 encoded_targets = one_hot_encoding(targets, targets_classes)
---
+++ exercises/librerias.jl
@@ -0,0 +1,4 @@
+using Statistics # Funciones estadísticas comunes para análisis de datos.
+using DelimitedFiles # Herramientas para leer y escribir archivos delimitados.
+using DataFrames # Estructuras de datos para manipular datos tabulares en Julia.
+using Flux # Permite entrenar Redes de Neuronas Artificiales en Julia.
