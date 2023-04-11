ed835c9f6df65639dee51c3af593dbda1d24ee22
Rodrigo Naranjo González <56093078+rng1@users.noreply.github.com>
2023-04-11 16:08:33 +0200

    Delete libraries.jl

--- exercises/libraries.jl
+++
@@ -1,3 +0,0 @@
-using Statistics # Funciones estadísticas comunes para análisis de datos.
-using DelimitedFiles # Herramientas para leer y escribir archivos delimitados.
-using DataFrames # Wstructuras de datos para manipular datos tabulares en Julia.
